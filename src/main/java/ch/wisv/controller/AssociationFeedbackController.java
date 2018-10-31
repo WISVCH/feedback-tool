b41ce7f018e8946e48c9faa990faf564b5b5ddde
Sven Popping <sven@popping.co>
2018-10-31 19:06:48 +0100

    Improve layout association form

--- src/main/java/ch/wisv/controller/AssociationFeedbackController.java
+++ src/main/java/ch/wisv/controller/AssociationFeedbackController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -40,7 +42,7 @@ public class AssociationFeedbackController {
     /**
      * Create new association feedback.
      */
-    @RequestMapping("/create")
+    @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("feedback", new AssociationFeedback());
         return "association/associationForm";
@@ -50,7 +52,7 @@ public class AssociationFeedbackController {
      * Save new association feedback.
      */
     @Transactional
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping(value = "/create")
     public String save(@Valid @ModelAttribute("feedback") AssociationFeedback associationFeedback,
                        BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {

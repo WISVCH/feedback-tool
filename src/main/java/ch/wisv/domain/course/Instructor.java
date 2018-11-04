1e0dce182c2a956fdc91e072c78e0ab6eac0054f
Sven Popping <sven@popping.co>
2018-11-04 20:28:21 +0100

    Add instructor encryption for email and name

--- src/main/java/ch/wisv/domain/course/Instructor.java
+++ src/main/java/ch/wisv/domain/course/Instructor.java
@@ -1,10 +1,14 @@
 package ch.wisv.domain.course;
 
+import ch.wisv.converters.StringCryptoConverter;
+import java.util.List;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * Created by Tom on 14/05/2017.
@@ -17,9 +21,11 @@ public class Instructor {
     private long id;
 
     @NotEmpty
+    @Convert(converter = StringCryptoConverter.class)
     private String name;
 
     @NotEmpty
+    @Convert(converter = StringCryptoConverter.class)
     private String mail;
 
     @ManyToMany(mappedBy = "instructors")

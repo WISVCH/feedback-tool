161bd4b2ff39b195dbaa71e6a88bc53e3138e599
Sven Popping <sven@popping.co>
2018-11-05 12:47:18 +0100

    Move concatenation and splitting to own method

--- src/main/java/ch/wisv/converters/AbstractCryptoConverter.java
+++ src/main/java/ch/wisv/converters/AbstractCryptoConverter.java
@@ -11,6 +11,8 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.persistence.AttributeConverter;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * AbstractCryptoConverter class.
@@ -52,7 +54,7 @@ abstract class AbstractCryptoConverter<T> implements AttributeConverter<T, Strin
             try {
                 Cipher cipher = cipherInitializer.prepareAndInitCipher(Cipher.ENCRYPT_MODE, DATABASE_ENCRYPTION_KEY, null);
 
-                return this.encrypt(cipher, attribute) + CONCATENATION + Base64.getEncoder().encodeToString(cipher.getIV());
+                return this.concatenatedCipherTextAndIv(this.encrypt(cipher, attribute), cipher.getIV());
             } catch (NoSuchAlgorithmException | InvalidKeyException | InvalidAlgorithmParameterException | BadPaddingException |
                     NoSuchPaddingException | IllegalBlockSizeException e) {
                 throw new RuntimeException(e);
@@ -73,13 +75,10 @@ abstract class AbstractCryptoConverter<T> implements AttributeConverter<T, Strin
     public T convertToEntityAttribute(String dbData) {
         if (isNotEmpty(DATABASE_ENCRYPTION_KEY) && isNotEmpty(dbData)) {
             try {
-                String[] splitDbData = dbData.split(CONCATENATION);
-                String cipherText = splitDbData[0];
-                byte[] iv = Base64.getDecoder().decode(splitDbData[1]);
+                Pair<String, byte[]> cipherTextAndIv = this.splitDbData(dbData);
+                Cipher cipher = cipherInitializer.prepareAndInitCipher(Cipher.DECRYPT_MODE, DATABASE_ENCRYPTION_KEY, cipherTextAndIv.getRight());
 
-                Cipher cipher = cipherInitializer.prepareAndInitCipher(Cipher.DECRYPT_MODE, DATABASE_ENCRYPTION_KEY, iv);
-
-                return this.decrypt(cipher, cipherText);
+                return this.decrypt(cipher, cipherTextAndIv.getLeft());
             } catch (NoSuchAlgorithmException | InvalidKeyException | InvalidAlgorithmParameterException | BadPaddingException |
                     NoSuchPaddingException | IllegalBlockSizeException e) {
                 throw new RuntimeException(e);
@@ -87,6 +86,33 @@ abstract class AbstractCryptoConverter<T> implements AttributeConverter<T, Strin
         }
 
         return this.stringToEntityAttribute(dbData);
+    }
+
+    /**
+     * Concatenated cipher text and IV together.
+     *
+     * @param cipherText of type String
+     * @param iv         of type byte[]
+     *
+     * @return String
+     */
+    private String concatenatedCipherTextAndIv(String cipherText, byte[] iv) {
+        return cipherText + CONCATENATION + Base64.getEncoder().encodeToString(iv);
+    }
+
+    /**
+     * Split db data into cipher text and IV.
+     *
+     * @param dbData of type String
+     *
+     * @return Pair
+     */
+    private Pair<String, byte[]> splitDbData(String dbData) {
+        String[] splitDbData = dbData.split(CONCATENATION);
+        String cipherText = splitDbData[0];
+        byte[] iv = Base64.getDecoder().decode(splitDbData[1]);
+
+        return new ImmutablePair<>(cipherText, iv);
     }
 
     /**

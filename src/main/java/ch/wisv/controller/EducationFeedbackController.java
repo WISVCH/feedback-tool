8e18401232bcdf381ada22c36e39d2cdbf1bd4e2
Jurriaan Den Toonder <jur.den.toonder@gmail.com>
2019-03-13 14:37:20 +0100

    Add google guava for ImmutableMap in EducationFeedbackController
    Signed-off-by: Jurriaan Den Toonder <jur.den.toonder@gmail.com>

--- src/main/java/ch/wisv/controller/EducationFeedbackController.java
+++ src/main/java/ch/wisv/controller/EducationFeedbackController.java
@@ -1,7 +1,6 @@
 package ch.wisv.controller;
 
 import ch.wisv.domain.course.Course;
-import ch.wisv.domain.feedback.AssociationFeedback;
 import ch.wisv.domain.feedback.EducationFeedback;
 import ch.wisv.service.CaptchaService;
 import ch.wisv.service.CourseService;
@@ -9,19 +8,16 @@ import ch.wisv.service.EducationFeedbackService;
 import ch.wisv.service.NotificationService;
 import ch.wisv.util.BindingResultBuilder;
 import com.google.common.collect.ImmutableMap;
-import java.util.HashMap;
-import javax.transaction.Transactional;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.util.HashMap;
 
 /**
  * Controller for education feedback.
